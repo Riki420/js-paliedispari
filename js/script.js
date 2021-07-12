bc5c768d775a420a4959c5138cf3657911c864b0
Riki420 <girottiriccardo1998@gmail.com>
2021-07-12 16:01:41 +0200

    if(userWord === userWordReverse)

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ Creare una funzione per capire se la parola inserita è palindroma */
 
 //chiedo la parola all'utente
 var userWord = prompt('inserisci una parola').toString();
-console.log('userWord:' + userWord );
+console.log('userWord: ' + userWord );
 
 //con split creo un array con le lettere della parola
 userWordSplit = userWord.split('');
-console.log('Parola divisa:' + userWordSplit);
+console.log('Parola divisa: ' + userWordSplit);
 
 //inverto il senso della lista appena creata
 userWordSplit = userWordSplit.reverse();
@@ -16,6 +16,13 @@ console.log('Lettere invertite: ' + userWordSplit);
 
 //riunisco le lettere per ottenere la stringa
 var userWordReverse = userWordSplit.join('');
-console.log('' + userWordReverse);
+console.log('Parola riunita: ' + userWordReverse);
 
+//verifico se la parola inserita dall'utente
+// è uguale alla parola invertita
 
+if(userWord == userWordReverse){
+    console.log('La parola è palindroma');
+  } else {
+    console.log('La parola non è palindroma');
+  }
