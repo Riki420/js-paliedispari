c3e2fe890262512616cfb8cc8886eac110a27a45
Riki420 <girottiriccardo1998@gmail.com>
2021-07-12 15:52:18 +0200

    creati contenuti HTML + inizio palindromo

---
+++ js/script.js
@@ -0,0 +1,14 @@
+/* Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma */
+
+//chiedo la parola all'utente
+var userWord = prompt('inserisci una parola').toString();
+console.log('userWord:' + userWord );
+
+//con split creo un array con le lettere della parola
+userWordSplit = userWord.split('');
+console.table(userWordSplit);
+
+
+
---
+++ js/script2.js
@@ -0,0 +1,15 @@
+/*Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+Sommiamo i due numeri
+Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+Dichiariamo chi ha vinto.*/
+
+
+//* 1 TRAMITE BOTTONI CHIEDO SE PARI O DISPARI
+//* 2 CHIEDO ALL'UTENTE UN NUMERO DA 1 A 5
+//* 3 GENERO UN NUMERO RANDOM PER IL COMPUTER
+//* 4 FACCIO LA SOMMA DEI DUE NUMERI
+//* 5 STABILISCO SE PARI O DISPARI
+//* 6 DICHIARO IL VINCITORE
+
